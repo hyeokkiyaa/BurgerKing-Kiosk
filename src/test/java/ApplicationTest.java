3d3b6f3d31c3ce2fd23cdb4ab79ccf09b300484b
hyeokkiyaa <khm38607574@gmail.com>
2024-12-31 22:16:27 +0900

    feat: add TestFile but Failed.. need to ask

--- src/test/java/ApplicationTest.java
+++ src/test/java/ApplicationTest.java
@@ -2,15 +2,24 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 import kiosk.Application;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class ApplicationTest extends NsTest {
 
     @Test
     void 음식입력확인() {
-
+        run("1","1","4","0","5");
+        assertThat(output()).contains("와퍼 1개", "6900원");
     }
 
     @Override
     protected void runMain() {
-        Application.main(new String[]{});
+        try {
+            Application.main(new String[]{});
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
