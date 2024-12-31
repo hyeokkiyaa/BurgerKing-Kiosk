c9a639ca2ab2aacfaa2a575036a14fc0ecae1508
hyeokkiyaa <khm38607574@gmail.com>
2024-12-31 22:04:29 +0900

    feat: build.gradle added for Testing

---
+++ src/test/java/ApplicationTest.java
@@ -0,0 +1,16 @@
+import camp.nextstep.edu.missionutils.test.NsTest;
+import kiosk.Application;
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTest extends NsTest {
+
+    @Test
+    void 음식입력확인() {
+
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+}
